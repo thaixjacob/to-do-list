0955999e53136c0b8b2915e56ca3b84e7450085a
`thais` <`tjlannes@gmail.com`>
2023-05-12 16:31:05 +0200

    create get controller

---
+++ src/server/controller/todo.ts
@@ -0,0 +1,12 @@
+import { read } from '@db-crud-todo';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+function get(_req: NextApiRequest, res: NextApiResponse) {
+  const ALL_TODOS = read();
+
+  res.status(200).json({ todos: ALL_TODOS });
+}
+
+export const todoController = {
+  get,
+};
