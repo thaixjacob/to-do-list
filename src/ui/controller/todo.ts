711b13f7aabc5fdd3dbbf1a81acc1df9245667b3
`thais` <`tjlannes@gmail.com`>
2023-05-23 13:10:36 +0200

    add summary code frontend

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -1,3 +1,10 @@
+// --> FRONTEND
+/*
+
+Makes an asynchronous request to the API '/api/todos' and handles the response received from the server.
+
+*/
+
 async function get() {
   return fetch('/api/todos').then(async (serverResponse) => {
     const todosString = await serverResponse.text()
