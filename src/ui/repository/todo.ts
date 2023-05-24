36af6cb8253a6e1cbc96550c9aeeb52db995ebd7
`thais` <`tjlannes@gmail.com`>
2023-05-24 12:24:09 +0200

    validate the date that is coming from the server in the repository

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -1,8 +1,9 @@
-// --> FRONTEND
+// --> FRONTEND (Repository - Takes the data from whence it comes and makes sure the data is correct)
 /* 
 
-Fonte da verdade do meu dado, onde o controller obtem a informaçao
-Perguntar: O que recebo e o que retorno?
+Source of truth of my data, where the controller gets the information
+Ask: What do I receive and what do I return?
+
 */
 
 interface TodoRepositoryGetParams {
@@ -19,17 +20,17 @@ interface TodoRepositoryGetOutput {
 function get({ page, limit }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
   return fetch('/api/todos').then(async (serverResponse) => {
     const todosString = await serverResponse.text()
-    const todosFromServer = JSON.parse(todosString).todos
+    const todosFromServer = parseTodosFromServer(JSON.parse(todosString)).todos
 
     const ALL_TODOS = todosFromServer
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
     const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex)
-    const totalPages = Math.ceil(ALL_TODOS.lenght / limit)
+    const totalPages = Math.ceil(ALL_TODOS.length / limit)
 
     return {
       todos: paginatedTodos,
-      total: ALL_TODOS.lenght,
+      total: ALL_TODOS.length,
       pages: totalPages,
     }
   })
@@ -45,4 +46,41 @@ interface Todo {
   content: string
   date: Date
   done: boolean
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+  //Returns an all object that has an array of all
+  //Validates if the data coming from the server is correct
+
+  if (
+    responseBody !== null &&
+    typeof responseBody === 'object' &&
+    'todos' in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo === null && typeof todo !== 'object') {
+          throw new Error('Invalid todo from API')
+        }
+
+        const { id, content, date, done } = todo as {
+          id: string
+          content: string
+          date: string
+          done: string
+        }
+
+        return {
+          id,
+          content,
+          done: String(done).toLowerCase() === 'true',
+          date: new Date(date),
+        }
+      }),
+    }
+  }
+  return {
+    todos: [],
+  }
 }
