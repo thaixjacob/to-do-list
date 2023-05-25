dd792d02126109e9c148a4490e7957f530760f8f
`thais` <`tjlannes@gmail.com`>
2023-05-25 16:31:33 +0200

    update backend files with pagination

--- src/server/controller/todo.ts
+++ src/server/controller/todo.ts
@@ -1,19 +1,31 @@
-// --> BACKEND (controller - Takes the data sources and defines the flow that things have to happen on the screen)
+// --> BACKEND (Controller - Takes the data sources and defines the flow that things have to happen on the screen)
 /*
 
-The code defines a CONTROLLER for the "GET" route of an API. When this route is accessed, the get function is executed.
-Within the get function, the read method of the @db-crud-todo module is called to get all the "alls" (tasks) from the database.
-An HTTP response is then sent with status 200 and the body containing a JSON object with the "all" property, which contains the fetched alls.
+When we send the code to the user's screen, what we will have is the output code coming out of the backend.
 
 */
 
-import { read } from '@db-crud-todo'
+import { todoRepository } from '@server/repository/todo'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-function get(_req: NextApiRequest, res: NextApiResponse) {
-  const ALL_TODOS = read()
+function get(req: NextApiRequest, res: NextApiResponse) {
+  const query = req.query
+  const page = Number(query.page)
+  const limit = Number(query.limit)
 
-  res.status(200).json({ todos: ALL_TODOS })
+  if (query.page && isNaN(page)) {
+    res.status(400).json({ error: { message: '`page` must be a number' } })
+    return
+  }
+
+  if (query.limit && isNaN(limit)) {
+    res.status(400).json({ error: { message: '`limit` must be a number' } })
+    return
+  }
+
+  const output = todoRepository.get({ page, limit })
+
+  res.status(200).json({ total: output.total, pages: output.pages, todos: output.todos })
 }
 
 export const todoController = {
---
+++ src/server/repository/todo.ts
@@ -0,0 +1,49 @@
+// --> BACKEND (Repository - Takes the data sources and defines the flow that things have to happen on the screen)
+/*
+
+Implements a repository for "all" related operations (tasks or items to be done) using a read mechanism called
+read provided by the @db-crud-todo module.
+
+*/
+import { read } from '@db-crud-todo'
+
+interface TodoRepositoryGetParams {
+  page?: number
+  limit?: number
+}
+
+interface TodoRepositoryGetOutput {
+  todos: Todo[]
+  total: number
+  pages: number
+}
+
+function get({ page, limit }: TodoRepositoryGetParams = {}): TodoRepositoryGetOutput {
+  const currentPage = page || 1
+  const currentLimit = limit || 10
+
+  const ALL_TODOS = read()
+
+  const startIndex = (currentPage - 1) * currentLimit
+  const endIndex = currentPage * currentLimit
+  const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex)
+  const totalPages = Math.ceil(ALL_TODOS.length / currentLimit)
+
+  return {
+    todos: paginatedTodos,
+    total: ALL_TODOS.length,
+    pages: totalPages,
+  }
+}
+
+export const todoRepository = {
+  get,
+}
+
+//Model/Schema
+interface Todo {
+  id: string
+  content: string
+  date: string
+  done: boolean
+}
