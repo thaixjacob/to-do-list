fc3eb618df77727f6c82f80a64303886df5767f6
`thais` <`tjlannes@gmail.com`>
2023-05-25 16:31:13 +0200

    update ui files transferring paging logic to the backend

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -1,18 +1,20 @@
-// --> FRONTEND
+// --> FRONTEND (Controller - Handles the response received from the server.)
 /*
 
-Makes an asynchronous request to the API '/api/todos' and handles the response received from the server.
+Defines a controller called todoController that has a get function. This function takes a page parameter,
+which represents the desired results page, and uses the todoRepository to fetch the data related to
+"todo" based on the given parameters.
 
 */
 
-import { todoRepository } from '@ui/repository/todo'
+import { todoRepository } from '@ui/repository/todo' //Accesses/manipulates the data of "todo"
 
 interface TodoControllerGetParams {
   page: number
 }
 
 async function get(params: TodoControllerGetParams) {
-  return todoRepository.get({ page: params.page, limit: 5 })
+  return todoRepository.get({ page: params.page, limit: 10 })
 }
 
 export const todoController = {
