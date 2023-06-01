51634e0410467ae4b1b54ff786e3ec9020700ad2
`thais` <`tjlannes@gmail.com`>
2023-06-01 11:53:32 +0200

    update summary of the custom error class

--- src/server/infra/errors.ts
+++ src/server/infra/errors.ts
@@ -1,3 +1,12 @@
+// --> BACKEND (Custom error class)
+/*
+
+This custom class is used to represent an error in an HTTP request. When instantiating an object of this class,
+a custom error is created with the message and status set. This allows the application to throw a specific error
+to indicate that a requested resource was not found.
+
+*/
+
 export class HttpNotFoundError extends Error {
   status: number
 
